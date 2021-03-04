21dc7f53efce39b8e6d68c3b16beb2d83866c297
Marc Adrian Peters <marcadrianpeters@gmail.com>
2021-03-04 23:25:15 +0100

    new js

--- script2.js
+++ script2.js
@@ -8,6 +8,66 @@ var friends = 0;
 var friends_reward_step = 10;
 var friends_price_base = 1.04;
 
+window.onclick=function(){
+    save();
+}
+
+//init
+window.onload=function(){
+    setInterval(autoclick,1000);
+    setInterval(save, 1000);
+
+    loadCsv();
+
+    //spielstand aus cache laden
+    var stored = localStorage['smak_clicker'];
+    if (stored){
+        myVar = JSON.parse(stored); 
+        eaten_smaks = myVar["eaten_smaks"];
+        clickpower = myVar["clickpower"];
+        friends = myVar["friends"];
+        update();
+    }
+}
+
+function loadCsv(){
+    $.ajax({
+        type: "GET",
+        url: "test.csv",
+        dataType: "text",
+        success: function(data){
+            var csvRows = [];
+            rows = data.replace(/(\r\n|\n|\r)/gm, ";");
+            rows = rows.split(';');
+            //console.log(rows);
+            for(var i = 0; i < rows.length; i++){
+                var temp = rows[i];
+                csvRows[i] = temp.split(',');
+            }
+            //console.log(csvRows);
+            csvArray = csvRows;
+        }
+    });
+}
+
+function save(){
+    var jstuff  = {
+        "eaten_smaks": eaten_smaks, 
+        "clickpower": clickpower,
+        "friends": friends
+    }
+
+    localStorage['smak_clicker'] = JSON.stringify(jstuff);
+}
+
+function reset(){
+    eaten_smaks=0;
+    clickpower=1;
+    friends=-1;
+    document.getElementById("section_friends").classList.add("d-none");
+    update();
+}
+
 //logarithm y to base x
 function getBaseLog(x, y) {
     return Math.log(y) / Math.log(x);
@@ -45,5 +105,16 @@ function update(){
         document.getElementById("button_buy_clickpower").disabled = true;
     } else {
         document.getElementById("button_buy_clickpower").disabled = false;
+    }
+}
+
+function debug(){
+    let button=document.getElementById("button_debug");
+    if(button.innerHTML=="mode"){
+        document.getElementById("debug").classList.remove("d-none");
+        button.innerHTML="debug mode";
+    }else{
+        document.getElementById("debug").classList.add("d-none");
+        button.innerHTML="debug mode";
     }
 }
